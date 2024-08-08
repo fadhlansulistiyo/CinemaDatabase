0d40c55a267763fb4c3a3b45ddd1f151bb094a45
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2024-08-08 14:42:01 +0700

    create new module core

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     id("androidx.room") version "2.6.0" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
+    alias(libs.plugins.android.library) apply false
 }
