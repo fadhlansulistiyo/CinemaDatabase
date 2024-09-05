b762e20c1de032858bd4e0f5fb581ebd7d2bf9f9
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2024-09-06 05:50:37 +0700

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.dynamic.feature) apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     id("androidx.room") version "2.6.0" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.android.dynamic.feature) apply false
 }
